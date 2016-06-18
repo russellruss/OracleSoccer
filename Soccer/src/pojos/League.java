0f720e71ea415f267a1ba2cb6573b7bdc9b4f9e0
Mozcalti <Mozcalti@192.168.1.67>
2016-06-18 13:56:24 -0500

    im back baby

--- Soccer/src/pojos/League.java
+++ Soccer/src/pojos/League.java
@@ -9,10 +9,10 @@ public class League {
     @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
         Player playerA1 = new Player(),playerA2=new Player(),playerB2=new Player(),playerB1=new Player();
-        playerA1.playerName = "Russell";
-        playerA2.playerName = "Alberto";
-        playerB1.playerName = "Jorge";
-        playerB2.playerName = "Eduardo";
+        playerA1.playerName = "Russell rodpal";
+        playerA2.playerName = "Alberto palma";
+        playerB1.playerName = "Jorge Perez";
+        playerB2.playerName = "Eduardo Rodriguez";
         
         Team Odin = new Team(), floki = new Team();
             Player[] team = {playerA1,playerA2};
@@ -24,15 +24,15 @@ public class League {
         floki.teamName = "FLOKI";
             floki.players=teamF;
             
-        System.out.println("EL equipo "+Odin.teamName+" se conforma de:");  
-        for (Player player : Odin.players) {
-            System.out.println("el jugador: "+player.playerName);    
-        }
+//        System.out.println("EL equipo "+Odin.teamName+" se conforma de:");  
+//        for (Player player : Odin.players) {
+//            System.out.println("el jugador: "+player.playerName);    
+//        }
         
-        System.out.println("EL equipo "+floki.teamName+" se conforma de:");  
-        for (Player player : floki.players) {
-            System.out.println("el jugador: "+player.playerName);    
-        }
+//        System.out.println("EL equipo "+floki.teamName+" se conforma de:");  
+//        for (Player player : floki.players) {
+//            System.out.println("el jugador: "+player.playerName);    
+//        }
         
         
         Game tourmalet = new Game();
@@ -54,7 +54,23 @@ public class League {
 
         tourmalet.goals[0].theTeam.teamName);
         
-        
+        for (Player player: floki.players) {
+            if(player.playerName.contains("Edu")){
+                System.out.println("ya lo encontre: "+ player.playerName);
+                System.out.println("con lastname: "+ player.playerName.split(" ")[1]);
+            }
+            
+        }
+        StringBuilder StepLName = new StringBuilder();
+        for (Player player  : floki.players) {
+            String[] splits = player.playerName.split(" ");
+            
+            StepLName.append(splits[1]);
+            StepLName.append(", ");
+            StepLName.append(splits[0]);
+            System.out.println(StepLName);
+            StepLName.delete(0, StepLName.length());
+        }
         
     }
     
